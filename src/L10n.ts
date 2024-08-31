18755b53267bc768a8c6158973b3d435cc798620
Andrea Condoluci <andreacondoluci@gmail.com>
2024-09-01 01:24:01 +0200

    add French language

--- src/L10n.ts
+++ src/L10n.ts
@@ -11,22 +11,41 @@ const locale_it: Locale = [
   ["Licensed under GNU GPLv3", "Licenza GNU GPLv3"],
 ];
 
+const locale_fr: Locale = [
+  ["Sync in progress", "Synchronisation en cours"],
+  ["Version", "Version"],
+  ["All lists", "Toutes les listes"],
+  ["Add", "Ajouter"],
+  ["New Expense List", "Nouvelle liste de dépenses"],
+  ["Join Existing", "Rejoindre une liste existante"],
+  ["There are no expense lists yet", "Il n'y a pas encore de liste de dépenses"],
+  ["Licensed under GNU GPLv3", "Sous licence GNU GPLv3"],
+];
+
+
 const locales: Map<string, Locale> = new Map([
   ["it", locale_it],
+  ["fr", locale_fr],
 ]);
-
-const lang = "en"; // FIXME: TODO:
 
 function lookup<A>(m: Map<A, A>, k: A): A {
   const v = m.get(k);
   return v ? v : k;
 }
 
-let _s: (str: string) => string = (str: string) => str;
-const g = locales.get(lang);
-if (g) {
-  const m = new Map(g);
-  _s = (str: string) => lookup(m, str);
+function getLocale(): (str: string) => string {
+  const languages = navigator.languages ?? [];
+  for (let language of languages) {
+    const languageCode = language.split("-")[0];
+    if (!languageCode) continue;
+    if (languageCode == "en") break;
+    const locale = locales.get(languageCode);
+    if (locale) {
+      const m = new Map(locale);
+      return (str: string) => lookup(m, str);
+    }
+  }
+  return (str: string) => str;
 }
 
-export const s = _s;
+export const s = getLocale();
